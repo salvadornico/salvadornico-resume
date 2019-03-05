93dd6ce206d3c0dd341934bbbe9a37ec1a3d2630
Nico Salvador <salvador.nico@gmail.com>
2019-03-05 18:03:25 +0800

    Added start script

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var resumePrompts = {
 }
 
 function main() {
-	console.log("Hello,My name is SilentLad and welcome to my resume")
+	console.log("Hello, I'm Nico, and this is my resume.")
 	resumeHandler()
 }
 
